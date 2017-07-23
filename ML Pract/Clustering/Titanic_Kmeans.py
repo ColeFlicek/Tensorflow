e9222987b22523210d6d44eaae4e619549878b83
Cole Desktop <cole.flicek@gmail.com>
2017-07-22 20:55:34 -0500

    Titanic Kmeans

--- ML Pract/Clustering/Titanic_Kmeans.py
+++ ML Pract/Clustering/Titanic_Kmeans.py
@@ -7,4 +7,16 @@ import pandas as pd
 
 style.use('ggplot')
 
+def Get_categories(dataframe, columns):
+
+    for col in columns:
+        dataframe[col] = dataframe[col].astype('category')
+        dataframe[col] = dataframe[col].cat.codes
+    print(df.head())
+
+
+
 df = pd.read_excel('titanic.xls')
+df.drop(['name', 'ticket', 'cabin', 'home.dest'], inplace = True)
+print(df.head())
+df = Get_categories(df, ['sex'])
